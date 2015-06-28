a93fd30856b8ccac93c06f9c433139ec766367eb
Thomas Lockney <thomas@lockney.net>
2015-06-27 23:02:26 -0700

    Updates

--- init.el
+++ init.el
@@ -1,84 +1,19 @@
-(require 'org-install)
-(require 'ob-tangle)
+;(require 'org-install)
+;(org-babel-load-file
+; (expand-file-name "readme.org" user-emacs-directory))
 
-(org-babel-load-file (concat user-emacs-directory "readme.org"))
+(defvar init-source-org-file (expand-file-name "readme.org" user-emacs-directory)
+  "The file that our emacs initialization comes form")
 
-;;(org-babel-tangle-file "/Users/tlockn/.emacs.d/readme.org" "/Users/tlockn/.emacs.d/readme.el" "emacs-lisp")
+(defvar init-source-el-file (expand-file-name "readme.el" user-emacs-directory)
+  "The file that our emacs initialization is generated into")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#657b83")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "31e1d95615b64ea08fad24a683b2bcf39f91c7826ff2a1ae614188194e1fe819" default)))
- '(fci-rule-color "#eee8d5")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#fdf6e3" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#586e75")
- '(highlight-tail-colors
-   (quote
-    (("#eee8d5" . 0)
-     ("#B4C342" . 20)
-     ("#69CABF" . 30)
-     ("#69B7F0" . 50)
-     ("#DEB542" . 60)
-     ("#F2804F" . 70)
-     ("#F771AC" . 85)
-     ("#eee8d5" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
- '(hl-fg-colors
-   (quote
-    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
- '(magit-diff-use-overlays nil)
- '(org-agenda-files
-   (quote
-    ("/Users/tlockn/Dropbox/org/notes.org" "/Users/tlockn/Dropbox/org/aggregates-requirements.org" "/Users/tlockn/Dropbox/org/journal.org" "/Users/tlockn/Dropbox/org/logbook-work.org" "/Users/tlockn/Dropbox/org/notes-db-connection-fix.org" "/Users/tlockn/Dropbox/org/todo.org")))
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
- '(term-default-bg-color "#fdf6e3")
- '(term-default-fg-color "#657b83")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c85d17")
-     (60 . "#be730b")
-     (80 . "#b58900")
-     (100 . "#a58e00")
-     (120 . "#9d9100")
-     (140 . "#959300")
-     (160 . "#8d9600")
-     (180 . "#859900")
-     (200 . "#669b32")
-     (220 . "#579d4c")
-     (240 . "#489e65")
-     (260 . "#399f7e")
-     (280 . "#2aa198")
-     (300 . "#2898af")
-     (320 . "#2793ba")
-     (340 . "#268fc6")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(if (file-exists-p init-source-org-file)
+  (if (and (file-exists-p init-source-el-file)
+           (time-less-p (nth 5 (file-attributes init-source-org-file)) (nth 5 (file-attributes init-source-el-file))))
+      (load-file init-source-el-file)
+    (if (fboundp 'org-babel-load-file) ; ' disqus syntax highlighting is lame
+        (org-babel-load-file init-source-org-file)
+      (message "Function not found: org-babel-load-file")
+      (load-file init-source-el-file)))
+  (error "Init org file '%s' missing." init-source-org-file))
