7e16a6f1010db2997ce73479486c852f66e9e958
Thomas Lockney <thomas@lockney.net>
2015-08-26 19:49:35 -0700

    revert to old init startup

--- init.el
+++ init.el
@@ -1,15 +1,12 @@
-(defvar init-source-org-file (expand-file-name "readme.org" user-emacs-directory)
-  "The file that our emacs initialization comes form")
 
-(defvar init-source-el-file (expand-file-name "readme.el" user-emacs-directory)
-  "The file that our emacs initialization is generated into")
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-(if (file-exists-p init-source-org-file)
-  (if (and (file-exists-p init-source-el-file)
-           (time-less-p (nth 5 (file-attributes init-source-org-file)) (nth 5 (file-attributes init-source-el-file))))
-      (load-file init-source-el-file)
-    (if (fboundp 'org-babel-load-file)
-        (org-babel-load-file init-source-org-file)
-      (message "Function not found: org-babel-load-file")
-      (load-file init-source-el-file)))
-  (error "Init org file '%s' missing." init-source-org-file))
+(require 'org-install)
+(require 'ob-tangle)
+
+(org-babel-load-file (concat user-emacs-directory "readme.org"))
+
